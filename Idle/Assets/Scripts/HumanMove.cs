e79c967eaf7d87419d27fd2003c9f088c8ead146
ridonebahadir <64854261+ridonebahadir@users.noreply.github.com>
2022-05-31 11:42:11 +0300

    Human Anim

--- Idle/Assets/Scripts/HumanMove.cs
+++ Idle/Assets/Scripts/HumanMove.cs
@@ -8,6 +8,7 @@ public class HumanMove : MonoBehaviour
     public float speed;
     public Animator anim;
     public Transform humanBody;
+    public Transform bag;
 
     // Update is called once per frame
     void Update()
@@ -28,13 +29,37 @@ public class HumanMove : MonoBehaviour
             Quaternion toRotation = Quaternion.LookRotation(movementDirection, Vector3.up);
             humanBody.rotation = Quaternion.RotateTowards(humanBody.rotation, toRotation, 720 * Time.deltaTime);
         }
-        if (Input.GetMouseButton(0))
+        if (bag.transform.childCount>0)
         {
-            anim.SetBool("Walk",true);
+            
+            if (Input.GetMouseButton(0))
+            {
+                anim.SetBool("Carry", true);
+                anim.SetBool("Walk", true);
+            }
+            else
+            {
+                anim.SetBool("Carry", true);
+                anim.SetBool("Walk", false);
+            }
         }
         else
         {
-            anim.SetBool("Walk", false);
+           
+            if (Input.GetMouseButton(0))
+            {
+                anim.SetBool("Carry", false);
+                anim.SetBool("Run", true);
+              
+            }
+            else
+            {
+                anim.SetBool("Carry", false);
+                anim.SetBool("Run", false);
+                anim.SetBool("Walk", false);
+               
+            }
         }
+       
     }
 }
