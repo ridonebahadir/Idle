1e6daabe99e0b145e00986eac78b7b647900a7df
ridonebahadir <64854261+ridonebahadir@users.noreply.github.com>
2022-07-01 19:06:57 +0300

    Hacizman

--- Idle/Assets/Scripts/Bot.cs
+++ Idle/Assets/Scripts/Bot.cs
@@ -6,9 +6,17 @@ using UnityEngine.AI;
 public class Bot : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
-    public Transform human;
+    public Transform target;
+    //public Transform[] machine;
+    public Transform startPos;
+    private Animator anim;
+    
     void Start()
     {
+        anim = GetComponent<Animator>();
+        startPos.position = transform.position;
+        //int a = Random.Range(0, machine.Length);
+        //target = machine[a];
         navMeshAgent = GetComponent<NavMeshAgent>();
         
     }
@@ -16,6 +24,16 @@ public class Bot : MonoBehaviour
    
     void Update()
     {
-        navMeshAgent.SetDestination(human.position);
+        navMeshAgent.SetDestination(target.position);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag=="Machine")
+        {
+            anim.SetBool("Carry", true);
+            Debug.Log("asdsad");
+            other.transform.parent = transform;
+            target = startPos;
+        }
     }
 }
