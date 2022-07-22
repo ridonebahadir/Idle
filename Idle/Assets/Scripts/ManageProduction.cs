930df0d368a4391aecf1513816638c8b3597af16
ridonebahadir <64854261+ridonebahadir@users.noreply.github.com>
2022-07-22 18:03:51 +0300

    Maddelerin yolu

--- Idle/Assets/Scripts/ManageProduction.cs
+++ Idle/Assets/Scripts/ManageProduction.cs
@@ -7,7 +7,8 @@ public class ManageProduction : MonoBehaviour
 {
     private Animator roboticAnim;
     public Transform[] breakPoints;
-    int turn;
+    public Transform hazne;
+    public int turn;
     int animTurn;
     bool oneTime;
     void Start()
@@ -20,17 +21,46 @@ public class ManageProduction : MonoBehaviour
     {
         if (breakPoints[turn].childCount>0)
         {
+           
             if (!oneTime)
             {
-                roboticAnim.SetInteger("Turn", animTurn);
-                if (animTurn<3)
-                {
-                    animTurn++;
-                    turn++;
-                }
-              
+
+                StartCoroutine(Run());
+                
+
+
                 oneTime = true;
             }
         }
+        
+    }
+  
+    IEnumerator Run()
+    {
+        
+        roboticAnim.SetInteger("Turn", animTurn);
+        GameObject obj = breakPoints[turn].GetChild(0).gameObject;
+       
+        yield return new WaitForSeconds(0.5f);
+        obj.transform.parent = hazne;
+        obj.transform.DOLocalMove(Vector3.zero,0.2f);
+        for (int i = 0; i < breakPoints[turn].childCount; i++)
+        {
+            breakPoints[turn].GetChild(i).DOLocalMove(new Vector3(0,i+1,0),0.5f);
+        }
+        yield return new WaitForSeconds(1f);
+        if (turn < 2)
+        {
+            animTurn++;
+            turn++;
+        }
+        else
+        {
+            animTurn = 0;
+            turn = 0;
+        }
+        oneTime = false;
+
+
     }
 }
