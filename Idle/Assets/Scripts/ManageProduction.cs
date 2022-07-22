5e2ecdc53ad1a77da2b496e622b0d733608c7154
ridonebahadir <64854261+ridonebahadir@users.noreply.github.com>
2022-07-22 18:28:00 +0300

    Animator düzeltme

--- Idle/Assets/Scripts/ManageProduction.cs
+++ Idle/Assets/Scripts/ManageProduction.cs
@@ -41,13 +41,15 @@ public class ManageProduction : MonoBehaviour
         roboticAnim.SetInteger("Turn", animTurn);
         GameObject obj = breakPoints[turn].GetChild(0).gameObject;
        
-        yield return new WaitForSeconds(0.5f);
+        yield return new WaitForSeconds(0.45f);
         obj.transform.parent = hazne;
         obj.transform.DOLocalMove(Vector3.zero,0.2f);
         for (int i = 0; i < breakPoints[turn].childCount; i++)
         {
             breakPoints[turn].GetChild(i).DOLocalMove(new Vector3(0,i+1,0),0.5f);
         }
+        yield return new WaitForSeconds(1f);
+        Destroy(obj);
         yield return new WaitForSeconds(1f);
         if (turn < 2)
         {
